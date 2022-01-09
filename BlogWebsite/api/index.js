99855e3662376008d996a5d9dc64c1fd3db384b7
SJuras <jurasstpko@gmailcom>
2022-01-09 14:22:34 +0100

    v1.2.3 models and routes added.

--- BlogWebsite/api/index.js
+++ BlogWebsite/api/index.js
@@ -1,5 +1,16 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+}).then(console.log("Connected!")).catch((err) => console.log(err));
+
 
 app.use("/", (req, res) => {
   console.log("ovo je main url")
