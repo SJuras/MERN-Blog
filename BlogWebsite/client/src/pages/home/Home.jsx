f77fa18ca64b3f9b52c8a03315e6303d57512384
SJuras <jurasstpko@gmailcom>
2022-01-13 10:08:01 +0100

    1.3.3 display all posts on home page functionality added.

--- BlogWebsite/client/src/pages/home/Home.jsx
+++ BlogWebsite/client/src/pages/home/Home.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import Header from '../../header/Header';
 import Sidebar from '../../sidebar/Sidebar';
 import Posts from "../../posts/Posts";
 import './Home.css';
+import axios from 'axios';
+import { useLocation } from "react-router";
 
 export default function Home() {
-  return(
+  const [posts, setPosts] = useState([]);
+
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get("/posts");
+      setPosts(res.data)
+    };
+    fetchPosts();
+  }, []);
+  return (
     <>
       <Header />
       <div className="home">
-        <Posts />
+        <Posts posts={posts} />
         <Sidebar />
       </div>
     </>
--- BlogWebsite/client/src/post/Post.jsx
+++ BlogWebsite/client/src/post/Post.jsx
@@ -2,31 +2,24 @@ import React from 'react'
 import './Post.css'
 import Ski from '../assets/ski.jpg'
 
-export default function Post(){
+export default function Post({post}){
   return(
     <div className="post">
-      <img src={Ski} alt="ski resort" className="postImg" />
+      {post.photo && (
+        <img src={post.photo} alt="ski resort" className="postImg" />
+      )}
       <div className="postInfo">
         <div className="postCats">
-          <span className="postCat">Music</span>
-          <span className="postCat">Life</span>
+          {post.categories.map((c) => (
+            <span className="postCat">{c.name}</span>
+          ))}
         </div>
-        <span className="postTitle">Lorem Ipsum</span>
+        <span className="postTitle">{post.title}</span>
         <hr />
-        <span className="postDate">1 hour ago</span>
+        <span className="postDate">{new Date(post.createdAt).toDateString()}</span>
       </div>
       <p className="postDesc">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis vestibulum ex,
-         sed bibendum sem. Nunc ac semper neque. Donec vel leo et lorem pharetra rhoncus sed quis
-          justo. Mauris iaculis enim quis nunc imperdiet ornare. Cras vitae nisl non turpis luctus
-          ultricies. Suspendisse mauris nisi, tempor nec tincidunt eu, pretium sed enim. Vestibulum
-          ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed vel ipsum
-          finibus, mattis neque non, iaculis felis. Etiam mi quam, congue at faucibus id, malesuada nec
-          purus. Morbi sodales nunc vel commodo dapibus. Vestibulum ornare, orci sit amet faucibus
-          vehicula, augue sapien feugiat sem, vitae viverra leo turpis quis diam. Praesent mattis
-          malesuada lorem, at scelerisque magna cursus ac. Fusce et arcu eros. Sed pharetra non
-          ligula eget molestie. Aliquam sit amet convallis ante. Suspendisse sollicitudin
-          scelerisque tristique.
+        {post.desc}
       </p>
     </div>
   );
