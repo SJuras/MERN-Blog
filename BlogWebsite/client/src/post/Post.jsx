f2d820391c6bbb333b2dc18157f35dfa7b44c823
SJuras <jurasstpko@gmailcom>
2022-01-13 12:25:25 +0100

    v1.3.4 click the post to get to the full post functionality added.

--- BlogWebsite/client/src/post/Post.jsx
+++ BlogWebsite/client/src/post/Post.jsx
@@ -1,20 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import './Post.css'
 import Ski from '../assets/ski.jpg'
 
 export default function Post({post}){
   return(
     <div className="post">
-      {post.photo && (
-        <img src={post.photo} alt="ski resort" className="postImg" />
-      )}
+      {/*
+        {post.photo && (
+          <img src={post.photo} alt="ski resort" className="postImg" />
+        )}
+        */}
+      {
+        post.photo ? <img src={post.photo} alt="ski" className="postImg" /> : <img src={Ski} alt="ski resort" className="postImg" />
+      }
       <div className="postInfo">
         <div className="postCats">
           {post.categories.map((c) => (
             <span className="postCat">{c.name}</span>
           ))}
         </div>
-        <span className="postTitle">{post.title}</span>
+        <Link to={`/post/${post._id}`} className="link">
+          <span className="postTitle">{post.title}</span>
+        </Link>
         <hr />
         <span className="postDate">{new Date(post.createdAt).toDateString()}</span>
       </div>
--- BlogWebsite/client/src/singlePost/SinglePost.jsx
+++ BlogWebsite/client/src/singlePost/SinglePost.jsx
@@ -1,44 +1,46 @@
 import React from 'react';
 import './SinglePost.css';
 import Ski from '../assets/ski.jpg';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router';
 
 import { FaEdit } from "react-icons/fa";
 import { FaTrashAlt } from "react-icons/fa";
 
 
 export default function SinglePost(){
+  const location = useLocation();
+  const path = location.pathname.split("/")[2];
+  const [post, setPost] = useState({})
+
+  useEffect(() => {
+    const getPost = async () => {
+      const res = await axios.get("/posts/"+path);
+      setPost(res.data)
+    };
+    getPost();
+  },[path])
+
   return(
     <div className="singlePost">
       <div className="singlePostWrapper">
-        <img src={Ski} alt="" className="singlePostImg" />
+      {
+        post.photo ? <img src={post.photo} alt="ski" className="postImg" /> : <img src={Ski} alt="ski resort" className="postImg" />
+      }
         <h1 className="singlePostTitle">
-          Lorem Ipsum
+          {post.title}
           <div className="singlePostEdit">
             <FaEdit style={{ cursor: "pointer", fontSize: "24px", color: "teal", marginRight: "12px" }} />
             <FaTrashAlt style={{ cursor: "pointer", fontSize: "24px", color: "tomato" }} />
           </div>
         </h1>
         <div className="singlePostInfo">
-          <span className="singlePostAuthor">Author: Janine</span>
-          <span className="singlePostDate">1 hour ago</span>
+          <span className="singlePostAuthor">Author: {post.username}</span>
+          <span className="singlePostDate">{new Date(post.createdAt).toDateString()}</span>
         </div>
         <p className="singlePostDesc">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
-          vestibulum arcu sit amet risus malesuada, a cursus ante commodo.
-          Donec nec sodales leo. Suspendisse potenti. Curabitur finibus non
-          augue id sodales. Morbi id facilisis felis. Maecenas magna justo,
-          consectetur sit amet augue quis, aliquet accumsan dui. Mauris vitae
-          rhoncus elit, a dignissim velit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
-          vestibulum arcu sit amet risus malesuada, a cursus ante commodo.
-          Donec nec sodales leo. Suspendisse potenti. Curabitur finibus non
-          augue id sodales. Morbi id facilisis felis. Maecenas magna justo,
-          consectetur sit amet augue quis, aliquet accumsan dui. Mauris vitae
-          rhoncus elit, a dignissim velit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
-          vestibulum arcu sit amet risus malesuada, a cursus ante commodo.
-          Donec nec sodales leo. Suspendisse potenti. Curabitur finibus non
-          augue id sodales. Morbi id facilisis felis. Maecenas magna justo,
-          consectetur sit amet augue quis, aliquet accumsan dui. Mauris vitae
-          rhoncus elit, a dignissim velit.
+          {post.desc}
         </p>
       </div>
     </div>
