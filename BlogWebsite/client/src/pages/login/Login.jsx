106ebb0547528bee674375d5f0f09880f4c4e31a
SJuras <jurasstpko@gmailcom>
2022-01-14 11:19:09 +0100

    v1.3.7 ContextAPI added.

--- BlogWebsite/client/src/pages/login/Login.jsx
+++ BlogWebsite/client/src/pages/login/Login.jsx
@@ -1,19 +1,27 @@
 import React from 'react'
+import { useRef } from 'react';
 import './Login.css'
 
 import { Link } from 'react-router-dom';
 
 
 export default function Login(){
+
+  const userRef = useRef();
+  const passwordRef = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
   return(
     <div className="login">
       <span className="loginTitle">Login</span>
-      <form className="loginForm">
-        <label>Email</label>
-        <input className="loginInput" type="email" placeholder="Enter your email... " />
+      <form className="loginForm" onSubmit={handleSubmit}>
+        <label>Username</label>
+        <input className="loginInput" type="text" placeholder="Enter your username... " ref={userRef} />
         <label>Password</label>
-        <input className="loginInput" type="password" placeholder="Enter your password... " />
-        <button className="loginButton">Login</button>
+        <input className="loginInput" type="password" placeholder="Enter your password... " ref={passwordRef} />
+        <button className="loginButton" type="submit">Login</button>
       </form>
       <button className="loginRegisterButton">
         <Link to="/register" className="link">Register</Link>
